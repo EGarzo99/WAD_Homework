d777b20c713c4cb1c3b138e6bc24825e95d38e45
Ander <carrascodelrio@ut.ee>
2025-11-14 12:13:23 +0200

    main.js, store.js

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from 'vuex'
+
+export default createStore({    
+    state: {
+        posts: [] // here posts will be stored
+        
+    },
+    getters: {
+        allPosts: (state) => state.posts // returns all posts from state
+    },
+    mutations: {
+        setPosts(state, posts) {
+            state.posts = posts // updates state with fetched posts
+        }
+    },
+    actions: { 
+        async fetchPosts({ commit }) {
+            try {
+                const response = await fetch('/json/posts.json')
+                const data = await response.json()
+                commit('setPosts', data) // calls mutation to update state
+            } catch (error) {
+                console.error('Error fetching posts:', error)
+            }
+        }
+    }
+})
