ae15feeb691834f10fd44e3e3b50b90f4409691d
Eneko Garzon <egarzon004@ikasle.ehu.eus>
2025-11-14 12:23:57 +0200

    mainpageView partialy done

---
+++ src/views/MainPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="layout">
+        <aside class='margin'></aside>
+        <main id="posts">
+            <!-- Posts will be dynamically inserted here by res/js/script.js -->
+        </main>
+        <aside class='margin'></aside>
+    </div>
+</template>
+
+<script>
+
+import Post from 'src/components/Post.vue'
+
+export default {
+  name: 'MainPage',
+  components: {
+    Post
+  }
+}
+</script>
+
+<style>     
+    .layout {
+        display: flex;       /* activa el modo fila */
+        gap: 20px;          /* espacio entre los elementos flexibles */
+    }
+
+    .margin {
+        flex: 1;             /* ocupa espacio flexible */
+        background: rgb(200, 200, 200);
+        border-radius: 15px;
+        margin: 20px auto;
+        max-width: 300px;    /* ancho máximo para que no desaparezca */
+    }
+</style>
