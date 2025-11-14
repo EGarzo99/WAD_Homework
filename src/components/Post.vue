444772eb0978c984a491dc678a8b4c5dc5e12244
Eneko Garzon <egarzon004@ikasle.ehu.eus>
2025-11-14 12:17:44 +0200

    Update Post.vue

---
+++ src/components/Post.vue
@@ -0,0 +1,14 @@
+<article class="post">
+    <div class="post-header">
+        <img src="res/images/user.png" alt="Profile" width="40">
+        <span class="date">Oct 22, 2022</span>
+    </div>
+    <br>
+    <div class="post-body">
+        <img src="res/images/tartu_plaza.jpg" alt="Tartu 2022" width="300">
+    <p>Tartu 2022</p>
+    </div>
+    <div class="post-footer">
+        <button>👍 Like</button>
+    </div>
+</article>
