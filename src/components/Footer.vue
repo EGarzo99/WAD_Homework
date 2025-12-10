469ab0287ba6079ea9fe4f60c3fc27781d744b1e
Eneko Garzon <egarzon004@ikasle.ehu.eus>
2025-12-10 18:19:16 +0200

    contactpage updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,6 @@
   <footer class="footer">
 
     <div class="footer-column">
-        <h3 class="footer-subtitle">Contact</h3>
-        <ul class="contact-list">
-            <li><span>📍</span> Delta Centre, Narva mnt 18, 51009 Tartu, Estonia</li>
-            <li><span>📧</span> <a href="mailto:blog@webappdev.com">blog@webappdev.com</a></li>
-        </ul>
         <p>© 2025 Web Application - All rights reserved</p>
     </div>
 
--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="layout">
-    <aside class="margin"></aside>
-    <aside class="margin"></aside>
+<div class="layout">
+  <aside class="margin"></aside>
+  <div class="content">
+    <h3 class="footer-subtitle">Contact</h3>
+    <ul class="contact-list">
+        <li><span>📍</span> Delta Centre, Narva mnt 18, 51009 Tartu, Estonia</li>
+        <li><span>📧</span> <a href="mailto:blog@webappdev.com">blog@webappdev.com</a></li>
+    </ul>
   </div>
+  <aside class="margin"></aside>
+</div>
 </template>
 
 <script>
@@ -16,5 +23,16 @@ export default {
 </script>
 
 <style scoped>
+    .layout {
+        display: flex;       /* activa el modo fila */
+        gap: 20px;          /* espacio entre los elementos flexibles */
+    }
 
+    .margin {
+        flex: 1;             /* ocupa espacio flexible */
+        background: rgb(200, 200, 200);
+        border-radius: 15px;
+        margin: 20px auto;
+        max-width: 300px;    /* ancho máximo para que no desaparezca */
+    }
 </style>
