d4cec8f0ddb2d2a8f3ba3b04a36c15bbdc3e6c1b
Ander <carrascodelrio@ut.ee>
2025-12-12 15:32:29 +0200

    initDB added

--- database.js
+++ database.js
@@ -8,4 +8,34 @@ const pool = new Pool({
     port: "5432"
 });
 
+//Setup DB tables if they do not exist
+async function initDB() {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS "Users" (
+        user_id SERIAL PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+      );
+    `);
+
+    // Tabla Posts
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS "Posts" (
+        post_id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES "Users"(user_id) ON DELETE CASCADE,
+        body TEXT NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        likes INT DEFAULT 0
+      );
+    `);
+
+    console.log('Database initialized: tables Users and Posts exist.');
+  } catch (err) {
+    console.error('Error initializing database:', err);
+  }
+}
+
+initDB();
+
 module.exports = pool;
